035015c9fdab0405a58f9c543dd464cf96f12d18
Raynard Brown <brown_raynard@icloud.com>
2024-08-09 14:00:44 -0500

    Method1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("what is your name.");
+           string name = Console.ReadLine(); // input
+           Console.WriteLine("what is your favorite show.");
+           string show = Console.ReadLine(); // input
+           Console.WriteLine("what is your favorite month of the year.");
+           string month = Console.ReadLine(); // input
+           Console.WriteLine("what is your favorite color.");
+           string color = Console.ReadLine(); // input
+           Console.WriteLine("who was your favorite pet.");
+           string pet = Console.ReadLine(); // input
+
+
+        }
+
+        public static void FavFive(string name)
+        {
+            Console.WriteLine("{month} was always her and {pet} favorite.");
+            Console.WriteLine("{name} and her both love {color} they always stay up watch {show}");
+
         }
     }
 }
